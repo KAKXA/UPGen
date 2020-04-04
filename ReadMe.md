0d47e2ea2348b7e32ff9928fb759e93dce56c158
ns <notanemail@notanemail.notanemail>
2020-04-04 15:22:19 +0800

    v1.5.0

--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,5 @@ v1.4.0 is the finished version of UPGen, I use it to encrypto all username and p
 v1.4.1 change the verification of seed from SHA224 to sum of the string.
 
 v1.4.2 hide input seed and output string.
+
+v1.5.0 refactor.new: generate a human name
--- UPGen.py
+++ UPGen.py
@@ -5,11 +5,59 @@ from Crypto.Hash import SHA512
 from Base94 import Base94
 from Contains import contains
 from StringSum import strSum
+from Alias import alias
 
+def inputR(output, default=None, f=None, hide=False):
+    newInput = getpass if hide else input
+    while True:
+        if not default:
+            temp = newInput(output + ':')
+        elif default:
+            temp = newInput(output + '(default(' + default + ')):')
+            if temp == '':
+                temp = default
+        if not f or f(temp):
+            return temp
+
+t = inputR('Enter type(id(i)password(p)name(n))',default='p', f=lambda x: x in {'i','p','n'})
+k = inputR('Enter keyword', default=None, f=None)
+if t == 'n':
+    m = k + t
+else:
+    d = inputR('Do not ban starting with a digit?(y/n)', default='y', f=lambda x: x in {'y','n'})
+    swd = False if d == 'n' else True
+    l = int(inputR('Enter length(between 6 and 256)', default='16', f=lambda x: int(x) in range(6, 257)))
+    if t == 'p':
+        w = inputR('With punctuations(y/n)', default='y', f=lambda x: x in {'y','n'})
+        wp = False if w == 'n' else True
+        s = inputR('Enter seed',default=None,f=strSum, hide=True)
+        m = k + t + s
+    elif t == 'i':
+        w = inputR('With punctuations(y/n)', default='n', f=lambda x: x in {'y','n'})
+        wp = False if w == 'n' else True
+        m = k + t
+
+h = SHA512.new()
+h.update(m.encode())
+
+if t == 'i' or t == 'p':
+    b = h.digest()
+    string = Base94(b, wp).decode()
+    for i in range(513 - l):
+        res = string[i: l + i]
+        if contains(res, withPunctuation=wp, startWithDigit=swd):
+            break
+elif t == 'n':
+    res = alias(int(h.hexdigest(),16))
+copy(res)
+print('Already copy it to clipboard.')
+
+
+'''
 while True:
-    t = input('Enter type(username(u) or password(p) or others(o))(default p):')
+    t = input('Enter type(id(i) or password(p) or name(n))(default p):')
     if t == '': t = 'p'
-    if t == 'u' or t == 'p' or t == 'o': break
+    if t == 'i' or t == 'p' or t == 'n': break
 
 while True:
     l = input('Enter length(between 1 and 256)(default 16):')
@@ -21,12 +69,13 @@ while True:
 
 k = input('Enter keyword:')
 
-while True:
-    w = input('With punctuations(y/n)(default y for p and default n for u and o):')
+while t == 'i' or t == 'p':
+    w = input('With punctuations(y/n)(default y for p and default n for i):')
     if w == '' and t == 'p': w = 'y'
-    elif w == '' and (t == 'u' or t == 'o'): w = 'n'
+    elif w == '' and t == 'i': w = 'n'
     if w == 'y' or w == 'n': break
-while True:
+
+while t == 'i' or t == 'p':
     d = input('Start with digits?(y/n)(default y)')
     if d == '': d = 'y'
     if d == 'y' or d == 'n': break
@@ -37,20 +86,24 @@ while t == 'p':
     if strSum(s):
         m = k + t + s
         break
-
-
 # if not a password, don't use seed to construct a result
-if t == 'u' or t == 'o':
+if t == 'i' or t == 'n':
     m = k + t
 
 a = SHA512.new()
 a.update(m.encode())
-b = a.digest()
-res = Base94(b, withPunctuation=False if w == 'n' else True).decode()
 
+if t == 'i' or t == 'p':
+    b = a.digest()
+    res = Base94(b, withPunctuation=False if w == 'n' else True).decode()
+    for i in range(513 - l):
+        if contains(res[i: l + i], withPunctuation= False if w == 'n' else True, startWithDigit= False if d == 'n' else True):
+            copy(res[i: l + i])
+            print('Already copy it to clipboard.')
+            break
+elif t == 'n':
+    b = int(a.hexdigest(),16)
+    copy(b)
+    print('Already copy it to clipboard.')
+    '''
 
-for i in range(513 - l):
-    if contains(res[i: l + i], withPunctuation= False if w == 'n' else True, startWithDigit= False if d == 'n' else True):
-        copy(res[i: l + i])
-        print('Already copy password to clipboard.')
-        break
